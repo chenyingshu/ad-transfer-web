d1decbe5ddf9ce81800e036e00ccccc363b46e71
SusanChen <susan.chen@aigens.com>
2019-11-26 02:35:18 +0800

    add obj folder; refine animation

--- js/cys.js
+++ js/cys.js
@@ -9,12 +9,34 @@ num_object++;
 object[cube_id] = new THREE.Mesh( geometry[cube_geo_id], material[cube_mat_id] );
 scene.add( object[cube_id] );
 
+cube_geo_id2 = num_geometry;
+num_geometry++;
+geometry[cube_geo_id2] = new THREE.SphereGeometry( 0.5, 32, 32 );
+cube_mat_id2 = num_material;
+num_material++;
+material[cube_mat_id2] = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+cube_id2 = num_object;
+num_object++;
+object[cube_id2] = new THREE.Mesh( geometry[cube_geo_id2], material[cube_mat_id2] );
+scene.add( object[cube_id2] );
+
+x_sign = 1;
+y_sign = 1;
+z_sign = 1;
+// animation_id = num_animation;
+// num_animation ++;
 var animate = function () {
-  animation[num_animation] = requestAnimationFrame( animate );
-  num_animation++;
+  requestAnimationFrame( animate );
 
   object[cube_id].rotation.x += 0.01;
   object[cube_id].rotation.y += 0.01;
+
+  object[cube_id2].position.x += x_sign * Math.floor(Math.random()*100)*0.0002;
+  object[cube_id2].position.y += y_sign * Math.floor(Math.random()*100)*0.0002;
+  object[cube_id2].position.z += z_sign * Math.floor(Math.random()*100)*0.0002;
+  if (Math.abs(object[cube_id2].position.x) > 2) x_sign = -x_sign;
+  if (Math.abs(object[cube_id2].position.y) > 2) y_sign = -y_sign;
+  if (Math.abs(object[cube_id2].position.z) > 2) z_sign = -z_sign;
 
   renderer.render( scene, camera );
 };
